7d2190209c877fd7843408a301cf56e80e18af1b
Jiri Vanek <jvanek@redhat.com>
2025-07-02 17:33:56 +0200

    App now traverse images

--- src/main/java/org/judovana/App.java
+++ src/main/java/org/judovana/App.java
@@ -1,10 +1,46 @@
 package org.judovana;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        List<String> all = Files.readAllLines(new File("imagesToTryInOfficeGenerator").toPath());
+        int i = 0;
+        int e = 0;
+        int l=0;
+        int p=0;
+        for (String path: all) {
+            i++;
+            if (path.startsWith("#")) {
+                System.out.println(i + "/"+all.size() + " skipped " + path);
+            } else {
+                System.out.println(i + "/"+all.size() + " reading " + path);
+                BufferedImage img = ImageIO.read(new File(path));
+                if (img == null) {
+                    e++;
+                    System.out.println("  unknown error! " + e);
+                    continue;
+                }
+                if (img.getWidth()>img.getHeight()) {
+                    l++;
+                    System.out.println("  landscape " + l);
+                } else {
+                    p++;
+                    System.out.println("  portrait " + p);
+                }
+            }
+        }
+        System.out.println("landscapes: " + l);
+        System.out.println("portraits: " + p);
+        System.out.println("errors: " + e);
     }
 }
